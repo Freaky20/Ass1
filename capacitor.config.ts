da273dbe160a65cf16d870c418e7dd398e58c882
Freaky20 <akashgoyaluk@gmail.com>
2022-11-22 11:38:04 +0000

    apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'Ass1',
+  appName: 'Shopping app plan',
   webDir: 'www',
   bundledWebRuntime: false
 };
